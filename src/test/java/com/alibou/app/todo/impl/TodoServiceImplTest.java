43b1f18ec22c0bc2ad20ad3ff7cd9d0e58c8778d
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-09-01 14:19:32 +0330

    Warming up

--- src/test/java/com/alibou/app/todo/impl/TodoServiceImplTest.java
+++ src/test/java/com/alibou/app/todo/impl/TodoServiceImplTest.java
@@ -1,5 +1,29 @@
 package com.alibou.app.todo.impl;
 
-class TodoServiceImplTest {
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("Todo Service Impl Unit Tests")
+class TodoServiceImplTest {
+    @Test
+    @DisplayName("Creating Todo Test")
+    void createTodo() {
+        fail();
+    }
+
+    @Test
+    void success() {
+
+    }
+
+    @Test
+    @Disabled
+    void ignored() {
+    }
 }
